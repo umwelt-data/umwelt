c37c9430e4c1928b1ebcbec894b48b6896360f1c
Jonathan Zong <jzong@mit.edu>
2025-01-25 14:35:26 -0500

    cleanup

--- src/components/viewer/sonification/audioUnit.tsx
+++ src/components/viewer/sonification/audioUnit.tsx
@@ -1,11 +1,9 @@
 import { For } from 'solid-js';
-import { useSonificationState } from '../../../contexts/sonification/SonificationStateContext';
 import { useUmweltSpec } from '../../../contexts/UmweltSpecContext';
 import { AudioUnitSpec } from '../../../types';
 import { TraversalFieldControl } from './traversalFieldControl';
 import { AudioUnitStateProvider } from '../../../contexts/sonification/AudioUnitStateContext';
 import { AudioScalesProvider } from '../../../contexts/sonification/AudioScalesContext';
-import { useAudioEngine } from '../../../contexts/sonification/AudioEngineContext';
 import { AudioUnitPlaybackControl } from './audioUnitPlaybackControl';
 import { describeField } from '../../../util/description';
 import { getFieldDef, resolveFieldDef } from '../../../util/spec';
@@ -17,19 +15,23 @@ export type AudioUnitProps = {
 export function AudioUnit(props: AudioUnitProps) {
   const [spec] = useUmweltSpec();
 
+  function AudioUnitEncodings() {
+    return Object.entries(props.audioUnitSpec.encoding).map(([propName, encoding]) => {
+      if (encoding) {
+        const fieldDef = getFieldDef(spec, encoding.field);
+        if (fieldDef) {
+          const resolvedFieldDef = resolveFieldDef(fieldDef, encoding);
+          return <div>{`${propName}: ${describeField(resolvedFieldDef)}`}</div>;
+        }
+      }
+    });
+  }
+
   return (
     <AudioScalesProvider encoding={props.audioUnitSpec.encoding}>
       <AudioUnitStateProvider audioUnitSpec={props.audioUnitSpec}>
+        {<AudioUnitEncodings />}
         <For each={props.audioUnitSpec.traversal}>{(traversalFieldDef) => <TraversalFieldControl traversalFieldDef={traversalFieldDef} />}</For>
-        {Object.entries(props.audioUnitSpec.encoding).map(([propName, encoding]) => {
-          if (encoding) {
-            const fieldDef = getFieldDef(spec, encoding.field);
-            if (fieldDef) {
-              const resolvedFieldDef = resolveFieldDef(fieldDef, encoding);
-              return `${propName}: ${describeField(resolvedFieldDef)}`;
-            }
-          }
-        })}
         <AudioUnitPlaybackControl unitName={props.audioUnitSpec.name} />
       </AudioUnitStateProvider>
     </AudioScalesProvider>
