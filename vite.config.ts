6300af0d35612f8eb7274ffeed280731fce3dc85
Jonathan Zong <jzong@mit.edu>
2025-02-05 15:58:10 -0500

    did it work

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  base: '/umwelt/editor/',
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
